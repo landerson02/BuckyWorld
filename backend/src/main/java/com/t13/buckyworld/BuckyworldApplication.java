e5b85825c4c063dbaa8a2283069032d2e7ca282a
Lucas Anderson <lcanderson4@wisc.edu>
2024-02-16 17:49:14 -0600

    Add starter code

---
+++ backend/src/main/java/com/t13/buckyworld/BuckyworldApplication.java
@@ -0,0 +1,13 @@
+package com.t13.buckyworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuckyworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuckyworldApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/t13/buckyworld/BuckyworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t13.buckyworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuckyworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
