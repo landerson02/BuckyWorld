e0a918d65fa667e2eb4afbdc284e2cc65fbb27b9
Trishika Mukkavalli <mukkavalli@wisc.edu>
2024-02-27 17:02:46 -0600

    modify position of arrows and added gray bar

--- frontend/app/userpage/page.tsx
+++ frontend/app/userpage/page.tsx
@@ -3,13 +3,15 @@
 import React, { useState } from 'react';
 
 export default function UserPage() {
-    // Dummy data import can be converted to dynamic import if necessary
+    // Dummy data 
     let data = require('../../data/dummy_data.json');
 
-    // Hardcoded for now, will change
-    const [name, setName] = useState('USERNAME');
-    const [points, setPoints] = useState(30); 
-    const [position, setPosition] = useState(56); 
+    
+
+    // Hardcoded name points position for now, will change
+    const [name, setName] = useState(data.Users[0].Username);
+    const [points, setPoints] = useState(data.Users[0].TotalPoints); 
+    const [position, setPosition] = useState(56); // Dummy leaderboard position
 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100 p-4">
@@ -22,8 +24,8 @@ export default function UserPage() {
             <p className="text-gray-600">{points} points</p>
             <p className="text-red-500 mt-2">---- #{position} on leaderboard ----</p>
 
-            {/* Buttons do not currently do anything when pressed*/}
-            {/*py makes the buttons more vertical, mt is for top margin*/}
+            {/* Buttons do not currently do anything when pressed */}
+            {/* py makes the buttons more vertical, mt is for top margin */}
             <button className="bg-green-500 text-white w-40 py-4 rounded-full mt-16">
                 Account
             </button>
@@ -35,9 +37,9 @@ export default function UserPage() {
             </button>
 
             {/* Arrows do not currently do anything, we need to modify*/}
-            <div className="flex justify-between absolute bottom-4 left-0 right-0 px-4">
-                <span className="text-gray-600">{"<"}</span>
-                <span className="text-gray-600">{">"}</span>
+            <div className="flex absolute bottom-0 left-0 py-8 space-x-7 right-0 px-6 bg-gray-300 text-3xl">
+                <span className="text-gray-600 ">{"<--"}</span>
+                <span className="text-gray-600">{"-->"}</span>
             </div>
         </div>
     );
