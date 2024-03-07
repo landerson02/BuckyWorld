7b3af4432172f1c9691b07ca9370496c5c511ca1
Lucas Anderson <lcanderson4@wisc.edu>
2024-03-06 22:06:56 -0600

    Add sign in page

---
+++ frontend/app/signin/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+import {useState} from "react";
+
+export default function Page() {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('')
+
+  const submit = () => {
+    console.log('submitted');
+    // TODO: Add sign in functionality
+  }
+
+  return (
+    <div className={'h-screen flex flex-col items-center'}>
+      <div className={'font-medium text-2xl pt-12'}>Welcome to</div>
+      <div className={'font-bold text-4xl italic text-[#FF5A64]'}>BuckyWorld</div>
+
+      <form className={'flex flex-col pt-20 text-2xl font-light'} onSubmit={submit}>
+        <label className={'text-xl'}>Username</label>
+        <input
+          className={'bg-[#7DB3E5]'}
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label className={'pt-4 text-xl'}>Password</label>
+        <input
+          className={'bg-[#7DB3E5]'}
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder=""
+        />
+        <button
+          className={'mt-12 rounded-2xl bg-[#66B566] text-xl text-white w-20 h-10 self-center'}
+          type="submit"
+        >Log In</button>
+      </form>
+
+      <div className={'pt-12 text-[#FF5A64] font-light'}>New Here?</div>
+      <a
+        href={'/signup'}
+        className={'border-2 border-[#FF5A64] rounded-2xl text-[#FF5A64] w-20 h-10 text-center flex flex-col justify-center'}
+      >Register</a>
+    </div>
+  )
+}
