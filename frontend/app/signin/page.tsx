196ec19fc1a8df80801b88246febd984fe893acc
Lucas Anderson <lcanderson4@wisc.edu>
2024-03-08 14:15:06 -0600

    Style sign in better

--- frontend/app/signin/page.tsx
+++ frontend/app/signin/page.tsx
@@ -1,11 +1,18 @@
 'use client'
-import {useState} from "react";
+import React, {useState} from "react";
 
 export default function Page() {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('')
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isBadCredentials, setIsBadCredentials] = useState(false);
 
-  const submit = () => {
+  const submitSignIn = (event: React.FormEvent) => {
+    event.preventDefault();
+    if(!username || !password) {
+      setIsBadCredentials(true);
+      return;
+    }
+    setIsBadCredentials(false);
     console.log('submitted');
     // TODO: Add sign in functionality
   }
@@ -15,22 +22,23 @@ export default function Page() {
       <div className={'font-medium text-2xl pt-12'}>Welcome to</div>
       <div className={'font-bold text-4xl italic text-[#FF5A64]'}>BuckyWorld</div>
 
-      <form className={'flex flex-col pt-20 text-2xl font-light'} onSubmit={submit}>
+      <form className={'flex flex-col pt-20 text-2xl font-light'} onSubmit={submitSignIn}>
         <label className={'text-xl'}>Username</label>
         <input
-          className={'bg-[#7DB3E5]'}
+          className={'bg-[#7DB3E5] rounded-md border focus:outline-none focus:ring-1 focus:ring-[#FF5A64] focus:border-[#FF5A64]'}
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
         <label className={'pt-4 text-xl'}>Password</label>
         <input
-          className={'bg-[#7DB3E5]'}
+          className={'bg-[#7DB3E5] rounded-md border focus:outline-none focus:ring-1 focus:ring-[#FF5A64] focus:border-[#FF5A64]'}
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder=""
         />
+        {isBadCredentials && <div className={'text-red-500 pt-4 font-light text-sm'}>Please enter a valid username and password</div>}
         <button
           className={'mt-12 rounded-2xl bg-[#66B566] text-xl text-white w-20 h-10 self-center'}
           type="submit"
